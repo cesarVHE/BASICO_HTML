16cf0205249216544b3b0db9956b8c0965e36283
cesarVHE <71098892+cesarVHE@users.noreply.github.com>
2026-04-15 19:01:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 var map = L.map("map").setView([24.1, -102], 6);
 
 // Cargar capa base de OpenStreetMap
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-}).addTo(map);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map)
